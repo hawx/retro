56e7e1ead94e3b985de7ff9569d0cabfcaf3277b
Joshua Hawxwell <m@hawx.me>
2017-01-22 16:33:38 +0000

    Simplify grouping by votes

--- app/src/Column.elm
+++ app/src/Column.elm
@@ -7,6 +7,7 @@ module Column exposing ( Column
 
 import Dict exposing (Dict)
 import Card exposing (Card)
+import List
 
 type alias Column =
     { id : String
@@ -31,29 +32,25 @@ updateCard : String -> (Card -> Card) -> Column -> Column
 updateCard cardId f column =
     { column | cards = Dict.update cardId (Maybe.map f) column.cards }
 
-
 cardsByVote : Dict String Column -> List (Int, List Card)
 cardsByVote columns =
+    Dict.values columns
+        |> List.map (.cards >> Dict.values)
+        |> List.concat
+        |> List.filter (.revealed)
+        |> groupBy (.votes)
+        |> Dict.toList
+        |> List.sortBy (\(a,_) -> -a)
+
+groupBy : (a -> comparable) -> List a -> Dict comparable (List a)
+groupBy classifier list =
     let
-        getList dict =
-            Dict.toList dict |> List.map (\(_,b) -> b)
+        insert_ x maybe =
+            Maybe.withDefault [] maybe
+                |> ((::) x)
+                |> Just
 
-        groupInsert x maybe =
-            case maybe of
-                Just list -> Just (x :: list)
-                Nothing -> Just [x]
-
-        group res list =
-            case list of
-                (x :: xs) -> group (Dict.update x.votes (groupInsert x) res) xs
-                [] -> res
+        into el acc =
+            Dict.update (classifier el) (insert_ el) acc
     in
-        getList columns
-            |> List.map (.cards)
-            |> List.map getList
-            |> List.concat
-            |> List.filter (.revealed)
-            |> group Dict.empty
-            |> Dict.toList
-            |> List.sortBy (\(a,_) -> a)
-            |> List.reverse
+        List.foldl into Dict.empty list
