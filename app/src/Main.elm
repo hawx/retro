2f735dc7a07fadc3f4d97624cf08db7539544892
Joshua Hawxwell <m@hawx.me>
2017-01-22 13:44:13 +0000

    Extrac Retro page

--- app/src/Main.elm
+++ app/src/Main.elm
@@ -13,15 +13,13 @@ import WebSocket
 import Json.Decode as Decode
 import Json.Decode.Pipeline as Pipeline
 import Json.Encode as Encode
-import Column exposing (Column)
-import Card exposing (Card, Content)
-import Retro exposing (Retro)
 import Sock
 import DragAndDrop
 import Html.Events.Extra as ExtraEvent
 import Navigation
 import Route
 import Page.Menu as Menu
+import Page.Retro as Retro
 
 main =
     Navigation.programWithFlags UrlChange
@@ -45,20 +43,13 @@ webSocketUrl flags =
 
 -- Model
 
-type alias CardDragging = (String, String)
-type alias CardOver = (String, Maybe String)
-
 type alias Model =
     { user : Maybe String
     , token : Maybe String
     , retroId : Maybe String
-    , retroList : Maybe (List String)
-    , retroName : String
-    , retro : Retro
-    , input : String
-    , dnd : DragAndDrop.Model CardDragging CardOver
     , flags : Flags
     , menu : Menu.Model
+    , retro : Retro.Model
     }
 
 init : Flags -> Navigation.Location -> (Model, Cmd Msg)
@@ -67,25 +58,25 @@ init flags location =
         (menuModel, menuCmd) =
             Menu.init
 
+        (retroModel, retroCmd) =
+            Retro.init
+
         (initModel, initCmd) =
             urlChange
                 location
                 { user = Nothing
                 , token = Nothing
                 , retroId = Nothing
-                , retroList = Nothing
-                , retroName = ""
-                , retro = Retro.empty
-                , input = ""
-                , dnd = DragAndDrop.empty
                 , flags = flags
                 , menu = menuModel
+                , retro = retroModel
                 }
     in
         initModel !
             [ initCmd
             , storageGet "id"
             , Cmd.map MenuMsg menuCmd
+            , Cmd.map RetroMsg retroCmd
             ]
 
 -- Update
@@ -96,14 +87,9 @@ port storageGot : (Maybe String -> msg) -> Sub msg
 
 type Msg = SetId (Maybe String)
          | Socket String
-         | ChangeInput String String
-         | CreateCard String
-         | SetStage Retro.Stage
-         | Reveal String String
-         | Vote String String
-         | DnD (DragAndDrop.Msg (String, String) (String, Maybe String))
          | UrlChange Navigation.Location
          | MenuMsg Menu.Msg
+         | RetroMsg Retro.Msg
 
 joinRetro : Model -> (Model, Cmd Msg)
 joinRetro model =
@@ -125,12 +111,15 @@ update msg model =
             in
                 { model | menu = menuModel } ! [ Cmd.map MenuMsg menuMsg ]
 
-
-        Vote columnId cardId ->
+        RetroMsg subMsg ->
             case model.user of
                 Just userId ->
-                    model ! [ Sock.vote (webSocketUrl model.flags) userId columnId cardId ]
-                _ ->
+                    let
+                        (retroModel, retroMsg) = Retro.update (webSocketUrl model.flags) userId subMsg model.retro
+                    in
+                        { model | retro = retroModel } ! [ Cmd.map RetroMsg retroMsg ]
+
+                Nothing ->
                     model ! []
 
         SetId (Just parts) ->
@@ -140,64 +129,15 @@ update msg model =
                 _ ->
                     { model | user = Nothing } ! []
 
-        SetStage stage ->
-            case model.user of
-                Just userId ->
-                    { model | retro = Retro.setStage stage model.retro } !
-                        [ Sock.stage (webSocketUrl model.flags) userId (toString stage) ]
-                _ ->
-                    model ! []
-
-        Reveal columnId cardId ->
-            case model.user of
-                Just userId ->
-                    model ! [ Sock.reveal (webSocketUrl model.flags) userId columnId cardId ]
-                _ ->
-                    model ! []
-
-        DnD subMsg ->
-            case model.user of
-                Just userId ->
-                    case DragAndDrop.isDrop subMsg model.dnd of
-                        Just ((columnFrom, cardFrom), (columnTo, maybeCardTo)) ->
-                            case model.retro.stage of
-                                Retro.Thinking ->
-                                    if columnFrom /= columnTo then
-                                        { model | dnd = DragAndDrop.empty } ! [ Sock.move (webSocketUrl model.flags) userId columnFrom columnTo cardFrom ]
-                                    else
-                                        model ! []
-
-                                Retro.Voting ->
-                                    case maybeCardTo of
-                                        Just cardTo ->
-                                            if cardFrom /= cardTo then
-                                                { model | dnd = DragAndDrop.empty } ! [ Sock.group (webSocketUrl model.flags) userId columnFrom cardFrom columnTo cardTo ]
-                                            else
-                                                model ! []
-                                        Nothing ->
-                                            model ! []
-
-                                _ ->
-                                    model ! []
-
-                        Nothing ->
-                            { model | dnd = DragAndDrop.update subMsg model.dnd } ! []
-
-                _ ->
-                    model ! []
-
-        ChangeInput columnId input ->
-            { model | input = String.trim input } ! []
-
-        CreateCard columnId ->
-            case model.user of
-                Just userId ->
-                    { model | input = "" } ! [ Sock.add (webSocketUrl model.flags) userId columnId model.input ]
-                Nothing ->
-                    model ! []
-
         Socket data ->
-            Sock.update data model socketUpdate
+            let
+                (retroModel, retroCmd) =
+                    Sock.update data model.retro Retro.socketUpdate
+
+                (newModel, newCmd) =
+                    Sock.update data model socketUpdate
+            in
+                { newModel | retro = retroModel } ! [ newCmd, Cmd.map RetroMsg retroCmd ]
 
         UrlChange location ->
             urlChange location model
@@ -217,66 +157,15 @@ urlChange location model =
         _ ->
             model ! []
 
-parseStage : String -> Maybe Retro.Stage
-parseStage s =
-    case s of
-        "Thinking" -> Just Retro.Thinking
-        "Presenting" -> Just Retro.Presenting
-        "Voting" -> Just Retro.Voting
-        "Discussing" -> Just Retro.Discussing
-        _ -> Nothing
 
 socketUpdate : (String, Sock.MsgData) -> Model -> (Model, Cmd Msg)
 socketUpdate (id, msgData) model =
     case msgData of
-        Sock.Stage { stage } ->
-            case parseStage stage of
-                Just s ->
-                    { model | retro = Retro.setStage s model.retro } ! []
-
-                Nothing ->
-                    model ! []
-
-        Sock.Card { columnId, cardId, revealed, votes } ->
-            let
-                card =
-                    { id = cardId
-                    , votes = votes
-                    , revealed = revealed
-                    , contents = [ ]
-                    }
-            in
-                { model | retro = Retro.addCard columnId card model.retro } ! []
-
-        Sock.Content { columnId, cardId, cardText } ->
-            let content =
-                    { id = ""
-                    , text = cardText
-                    , author = id
-                    }
-            in
-                { model | retro = Retro.addContent columnId cardId content model.retro } ! []
-
-        Sock.Column { columnId, columnName, columnOrder } ->
-            let
-                column = { id = columnId, name = columnName, order = columnOrder, cards = Dict.empty }
-            in
-                { model | retro = Retro.addColumn column model.retro } ! []
-
-        Sock.Move { columnFrom, columnTo, cardId } ->
-            { model | retro = Retro.moveCard columnFrom columnTo cardId model.retro } ! []
-
-        Sock.Reveal { columnId, cardId } ->
-            { model | retro = Retro.revealCard columnId cardId model.retro } ! []
-
-        Sock.Group { columnFrom, cardFrom, columnTo, cardTo } ->
-            { model | retro = Retro.groupCards (columnFrom, cardFrom) (columnTo, cardTo) model.retro } ! []
-
-        Sock.Vote { columnId, cardId } ->
-            { model | retro = Retro.voteCard columnId cardId model.retro } ! []
-
         Sock.Error { error } ->
             handleError error model
+
+        _ ->
+            model ! []
 
 
 handleError : String -> Model -> (Model, Cmd Msg)
@@ -288,6 +177,8 @@ handleError error model =
         _ ->
             model ! []
 
+
+
 -- View
 
 view : Model -> Html Msg
@@ -296,13 +187,13 @@ view model =
         Just userId ->
             if model.retroId == Nothing then
                 Html.div []
-                    [ retroView userId model
+                    [ Html.map RetroMsg (Retro.view userId model.retro)
                     , footer
                     , retroListModal model
                     ]
             else
                 Html.div []
-                    [ retroView userId model
+                    [ Html.map RetroMsg (Retro.view userId model.retro)
                     , footer
                     ]
         Nothing ->
@@ -310,15 +201,6 @@ view model =
                 [ footer
                 , signInModal
                 ]
-
-retroView : String -> Model -> Html Msg
-retroView userId model =
-    Html.section [ Attr.class "section" ]
-        [ Html.div [ Attr.class "container is-fluid" ]
-              [ tabsView model.retro.stage
-              , columnsView userId model.retro.stage model.dnd model.retro.columns
-              ]
-        ]
 
 
 retroListModal : Model -> Html Msg
@@ -348,206 +230,6 @@ footer =
               ]
         ]
 
-
-tabsView : Retro.Stage -> Html Msg
-tabsView stage =
-    let
-        tab s =
-            Html.li [ Attr.classList [("is-active", stage == s)]
-                    , Event.onClick (SetStage s)
-                    ]
-                [ Html.a [] [ Html.text (toString s) ]
-                ]
-    in
-        Bulma.tabs [ Attr.class "is-toggle" ]
-            [ Html.ul [ Attr.class "is-left" ]
-                  [ tab Retro.Thinking
-                  , tab Retro.Presenting
-                  , tab Retro.Voting
-                  , tab Retro.Discussing
-                  ]
-            , Html.ul [ Attr.class "is-right" ]
-                [ Html.li []
-                      [ Html.a [ Attr.href (Route.toUrl Route.Menu) ]
-                            [ Html.text "Quit" ]
-                      ]
-                ]
-            ]
-
-fst (a, b) = a
-snd (a, b) = b
-
-columnsView : String -> Retro.Stage -> DragAndDrop.Model CardDragging CardOver -> Dict String Column -> Html Msg
-columnsView connId stage dnd columns =
-    if stage == Retro.Discussing then
-        let
-            getList : Dict comparable a -> List a
-            getList dict = Dict.toList dict |> List.map snd
-
-            cards : List Card
-            cards = getList columns
-                  |> List.map (.cards)
-                  |> List.map getList
-                  |> List.concat
-
-            cardsByVote : List (Int, List Card)
-            cardsByVote = cards
-                        |> group Dict.empty
-                        |> Dict.toList
-                        |> List.sortBy fst
-                        |> List.reverse
-
-            cardToView card =
-                Bulma.card []
-                      [ Bulma.cardContent [] [ contentsView card.contents ]
-                      ]
-
-            groupInsert : a -> Maybe (List a) -> Maybe (List a)
-            groupInsert x maybe =
-                case maybe of
-                    Just list -> Just (x :: list)
-                    Nothing -> Just [x]
-
-            group : Dict Int (List Card) -> List Card -> Dict Int (List Card)
-            group res list =
-                case list of
-                    (x :: xs) -> group (Dict.update x.votes (groupInsert x) res) xs
-                    [] -> res
-
-            columnView : (Int, List Card) -> Html Msg
-            columnView (vote, cards) =
-                Bulma.column []
-                    (titleCardView (toString vote) :: List.map cardToView cards)
-
-        in
-            cardsByVote
-                |> List.map columnView
-                |> Bulma.columns [ Attr.class "is-multiline" ]
-
-    else
-        Dict.toList columns
-            |> List.sortBy (snd >> .order)
-            |> List.map (columnView connId stage dnd)
-            |> Bulma.columns [ ]
-
-columnView : String -> Retro.Stage -> DragAndDrop.Model CardDragging CardOver -> (String, Column) -> Html Msg
-columnView connId stage dnd (columnId, column) =
-    let
-        title = [titleCardView column.name]
-        list =
-            Dict.toList column.cards
-                |> List.map (cardView connId stage dnd columnId)
-                |> List.concat
-        add = [addCardView columnId]
-    in
-        case stage of
-            Retro.Thinking ->
-                Bulma.column ([ Attr.classList [ ("over", dnd.over == Just (columnId, Nothing))
-                                              ]
-                             ] ++ DragAndDrop.dropzone DnD (columnId, Nothing))
-                    (title ++ list ++ add)
-
-            _ ->
-                Html.div [ Attr.class "column" ]
-                    (title ++ list)
-
-contentView : Content -> Html Msg
-contentView content =
-    Bulma.content []
-        [ Html.p [ Attr.class "title is-6" ] [ Html.text content.author ]
-        , Html.p [] [ Html.text content.text ]
-        ]
-
-contentsView : List Content -> Html Msg
-contentsView contents =
-    contents
-        |> List.map (contentView)
-        |> List.intersperse (Html.hr [] [])
-        |> Bulma.content []
-
-cardView : String -> Retro.Stage -> DragAndDrop.Model CardDragging CardOver -> String -> (String, Card) -> List (Html Msg)
-cardView connId stage dnd columnId (cardId, card) =
-    let
-        content = contentsView card.contents
-    in
-        case stage of
-            Retro.Thinking ->
-                if Card.authored connId card then
-                    [ Bulma.card (DragAndDrop.draggable DnD (columnId, cardId))
-                          [ Bulma.cardContent [] [ content ] ]
-                    ]
-                else
-                    []
-
-            Retro.Presenting ->
-                if not card.revealed then
-                    if Card.authored connId card then
-                        [ Bulma.card [ Attr.classList [ ("not-revealed", not card.revealed)
-                                                      , ("can-reveal", True)
-                                                      ]
-                                     , Event.onClick (Reveal columnId cardId)
-                                     ]
-                              [ Bulma.cardContent [] [ content ] ]
-                        ]
-                    else
-                        []
-                else
-                    [ Bulma.card []
-                          [ Bulma.cardContent [] [ content ] ]
-                    ]
-
-            Retro.Voting ->
-                [ Bulma.card (List.concat
-                                  [ DragAndDrop.draggable DnD (columnId, cardId)
-                                  , DragAndDrop.dropzone DnD (columnId, Just cardId)
-                                  , [ Attr.classList [ ("over", dnd.over == Just (columnId, Just cardId))
-                                                     , ("not-revealed", not card.revealed)
-                                                     ]
-                                    ]
-                                  ])
-
-                      [ Bulma.cardContent []
-                            [ content ]
-                      , Bulma.cardFooter []
-                          [ Bulma.cardFooterItem [] (toString card.votes)
-                          , Bulma.cardFooterItem [ Event.onClick (Vote columnId cardId) ] "Vote"
-                          ]
-                      ]
-                ]
-
-            _ ->
-                if card.revealed || Card.authored connId card then
-                    [ Bulma.card [ Attr.classList [ ("not-revealed", not card.revealed) ]
-                                 ]
-                          [ Bulma.cardContent [] [ content ] ]
-                    ]
-                else
-                    []
-
-
-titleCardView : String -> Html Msg
-titleCardView title =
-    Html.div [ Attr.class "not-card card-content has-text-centered" ]
-        [ Html.div [ Attr.class "content" ]
-              [ Html.h1 []
-                    [ Html.text title
-                    ]
-              ]
-        ]
-
-addCardView : String -> Html Msg
-addCardView columnId =
-    Bulma.card []
-        [ Bulma.cardContent []
-              [ Bulma.content []
-                    [ Html.textarea [ Event.onInput (ChangeInput columnId)
-                                    , ExtraEvent.onEnter (CreateCard columnId)
-                                    , Attr.placeholder "Add a card..."
-                                    ]
-                          [ ]
-                    ]
-              ]
-        ]
 
 -- Subscriptions
 
