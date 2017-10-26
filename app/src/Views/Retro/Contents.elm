84f0ebdd3913085780e1ecd2fdbf06f35a7f50b9
Joshua Hawxwell <m@hawx.me>
2017-10-26 18:12:36 +0100

    Use hash of card's author, this probably isn't the best

--- app/src/Views/Retro/Contents.elm
+++ app/src/Views/Retro/Contents.elm
@@ -1,6 +1,8 @@
 module Views.Retro.Contents exposing (view)
 
+import Bitwise
 import Bulma
+import Char
 import Data.Content exposing (Content)
 import Html exposing (Html)
 import Html.Attributes as Attr
@@ -17,6 +19,32 @@ view contents =
 contentView : Content -> Html msg
 contentView content =
     Bulma.content []
-        [ Html.p [ Attr.class "title is-6" ] [ Html.text content.author ]
+        [ Html.p [ Attr.class "title is-6" ]
+            [ Html.span
+                [ Attr.class "tag"
+                , Attr.style [ ( "background-color", stringToColour content.author ) ]
+                ]
+                []
+            , Html.span [] [ Html.text "  " ]
+            , Html.text content.author
+            ]
         , Html.p [] [ Html.text content.text ]
         ]
+
+
+stringToColour : String -> String
+stringToColour str =
+    let
+        hashChar char prev =
+            Char.toCode char + (Bitwise.shiftLeftBy 5 prev - prev)
+
+        hash =
+            String.foldl hashChar 0 str
+
+        part i =
+            Bitwise.and (Bitwise.shiftRightBy (i * 8) hash) 0xFF
+                |> toString
+                |> String.padLeft 2 '0'
+                |> (\x -> String.slice -2 (String.length x) x)
+    in
+    "#" ++ part 0 ++ part 1 ++ part 2
