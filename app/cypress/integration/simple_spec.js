6d01ba4c4b67047c262dbd5140ab086f1e462bdb
Joshua Hawxwell <m@hawx.me>
2017-12-22 15:49:24 +0000

    Add first cypress test

---
+++ app/cypress/integration/simple_spec.js
@@ -0,0 +1,14 @@
+describe('retro', function() {
+  it('visits the app', function() {
+    cy.visit('http://localhost:8080');
+
+    cy.contains('Sign-in with Test').click();
+
+    cy.contains('test@example.com');
+
+    cy.get('input').type('First Retro');
+    cy.get('#create').click();
+
+    cy.get('#open').click();
+  });
+});
