0d62daea12a706f678ccceeed1c8871e6360c566
Joshua Hawxwell <m@hawx.me>
2017-12-22 16:24:54 +0000

    Test edit card

--- app/cypress/integration/simple_spec.js
+++ app/cypress/integration/simple_spec.js
@@ -1,22 +1,38 @@
-describe('retro', function() {
-  it('visits the app', function() {
+describe('retro', () => {
+  it('can sign-in', () => {
     cy.visit('http://localhost:8080/reset');
 
     cy.contains('Sign-in with Test').click();
 
     cy.contains('test@example.com');
+  });
 
+  it('creates a new retro', () => {
     cy.get('input').type('First Retro');
     cy.get('#create').click();
 
     cy.get('#open').click();
+  });
 
+  it('adds a new card', () => {
     cy.get('.column:first-child').within((column) => {
       cy.get('textarea').type('halp');
       cy.get('a').click();
       cy.get('.card').its('length').should('eq', 2);
       cy.get('.card').contains('halp');
+    });
+  });
 
+  it('edits the card', () => {
+    cy.get('.column:first-child .card:not(.add-card)').within((column) => {
+      cy.get('.card-content').dblclick();
+      cy.get('textarea').type('pls{enter}');
+      cy.root().contains('halppls');
+    });
+  });
+
+  it('deletes the card', () => {
+    cy.get('.column:first-child').within((column) => {
       cy.get('.card .delete').click();
       cy.get('.card').its('length').should('eq', 1);
     });
